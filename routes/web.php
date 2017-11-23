a2fdbac2969f00bcdbc7e0a1799ce2346be257c1
Unknown <timoski@outlook.com>
2017-11-23 11:45:21 +0100

    finish redirect

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,49 @@
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
+
+Route::group(['middleware'=>'auth'], function(){
+
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('home');
+
 });
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/home', 'HomeController@index');
+
+
+
+if(env('APP_ENV') == 'production')
+{
+    Auth::routes();
+}
+elseif (env('APP_ENV') == 'local') 
+{
+    Route::get('/login', function(){return view('debug.login');});
+    Route::post('/login', function(){
+
+        $id = 1;
+		$user = \App\User::find($id);
+
+		if(!$user){
+			$user = new \App\User();
+			$user->id = $id;
+			$user->name = "test_Acount";
+            $user->email = 'test_Acount@rocwb.nl';
+            $user->password = "1234567890";
+			$user->save();
+		}
+
+        \Auth::login($user);
+		return redirect()->route('home');
+    
+    })->name('login');
+
+	Route::get('logout', function(){
+		Auth::logout();
+		return redirect()->route('login');
+	});
+}
