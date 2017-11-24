c3f08cd911624baec66d23c3a828965a15a20746
w9009 <woutermevius@live.nl>
2017-11-24 09:57:17 +0100

    Fixed style
    Login Register navbar fixed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,13 +2,13 @@
 
 @section('hero-content')
 
-<!-- <img class="mySlides" src="img/hero_scherem_1.jpeg" alt="">
+<img class="mySlides" src="img/hero_scherem_1.jpeg" alt="">
 <img class="mySlides" src="img/hero_scherem_2.jpeg" alt="">
 <img class="mySlides" src="img/hero_scherem_3.jpeg" alt="">
-<img class="mySlides" src="img/hero_scherem_4.jpeg" alt=""> -->
+<img class="mySlides" src="img/hero_scherem_4.jpeg" alt="">
 
 @endsection
 
 @section('content')
 
-@endsection('content')
+@endsection
