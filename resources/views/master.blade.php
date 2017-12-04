8a3385a4c0eccd6207f45c8f6b789faa3010f725
Sjoerdh95 <sjoerdh.sh@gmail.com>
2017-12-04 09:17:40 +0100

    fix

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -126,7 +126,7 @@
     <!--/.footer-bottom--> 
 </footer>
     <script src="https://cdn.jsdelivr.net/jsbarcode/3.6.0/JsBarcode.all.min.js"> </script>
-    <script> JsBarcode("#barcode", "{{$ticket}}");</script>
+    <script> JsBarcode("#barcode", "bla");</script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 	<script src="js/slider.js" type="text/javascript"></script>
 </body>
