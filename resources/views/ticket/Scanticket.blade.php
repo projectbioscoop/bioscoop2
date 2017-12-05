3a8ee0a4e6029adfee5916a3067acc978505d670
Sjoerdh95 <sjoerdh.sh@gmail.com>
2017-12-05 11:35:40 +0100

    style update checkticket

--- resources/views/ticket/Scanticket.blade.php
+++ resources/views/ticket/Scanticket.blade.php
@@ -1,1 +1,1 @@
-@extends('master')@section('content')    <div class="container">        <div class="scan">            <form action="">                <input type="text">                <input type="submit">            </form>        </div>    </div>@endsection
+@extends('master')@section('content')    <div class="container">        <div class="scan">            <form action="">                <input type="text" class="form-control">                <input type="submit" class="form-control">            </form>        </div>    </div>@endsection
