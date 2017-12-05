9fb1c2bb7f57ef445012178cfdde49a2296ce520
Unknown <D201664@edu.rocwb.nl>
2017-12-05 11:35:51 +0100

    fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('voornaam');
+            $table->string('firstname');
             $table->string('achternaam');
             $table->string('tussenVoegsel');
             $table->string('email')->unique();
